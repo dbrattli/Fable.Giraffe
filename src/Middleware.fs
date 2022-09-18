a542b9eec3f6c6160b4368d21f24053f4a1e37dd
Dag Brattli <dag@brattli.net>
2022-09-18 15:03:33 +0200

    Better logging

--- src/Middleware.fs
+++ src/Middleware.fs
@@ -14,53 +14,69 @@ module GiraffeMiddleware =
     let useLogger (provider: ILoggerProvider) = loggerFactory.AddProvider(provider)
 
     let useGiraffe (handler: HttpHandler) : App =
-        // pre-compile the handler pipeline
-        let func: HttpFunc = handler earlyReturn
-
         let defaultHandler =
             setStatusCode 404
             |> HttpHandler.text "Not found!"
 
-        let defaultFunc: HttpFunc = defaultHandler earlyReturn
+        // pre-compile the handler pipeline
+        let func: HttpFunc =
+            choose [ handler; defaultHandler ] earlyReturn
+
+        let defaultFunc: HttpFunc =
+            defaultHandler earlyReturn
 
         let services = ServiceCollection()
 
         // Setup logging
-        let logger = loggerFactory.CreateLogger("Giraffe")
+        let logger =
+            loggerFactory.CreateLogger("Giraffe")
+
         services.AddSingleton(logger)
 
         logger.LogInformation("Giraffe ASGI middleware initialized")
 
-        let app (scope: Scope) (receive: unit -> Task<Response>) (send: Request -> Task<unit>) = task {
-            let ctx = HttpContext(scope, receive, send, services)
+        let app (scope: Scope) (receive: unit -> Task<Response>) (send: Request -> Task<unit>) =
+            task {
+                let ctx =
+                    HttpContext(scope, receive, send, services)
 
-            if ctx.Request.Protocol = "http" then
-                let start = Diagnostics.Stopwatch.GetTimestamp()
+                if ctx.Request.Protocol = "http" then
+                    let start =
+                        Diagnostics.Stopwatch.GetTimestamp()
 
-                let! result = func ctx
+                    let! result = func ctx
 
-                if logger.IsEnabled LogLevel.Debug then
-                    let freq = double System.Diagnostics.Stopwatch.Frequency
-                    let stop = Diagnostics.Stopwatch.GetTimestamp()
-                    let elapsedMs = (double (stop - start)) * 1000.0 / freq
+                    if logger.IsEnabled LogLevel.Debug then
+                        let freq =
+                            double System.Diagnostics.Stopwatch.Frequency
 
-                    logger.LogDebug(
-                        "Giraffe returned {SomeNoneResult} for {HttpProtocol} {HttpMethod} at {Path} in {ElapsedMs}",
-                        (if result.IsSome then "Some" else "None"),
-                        ctx.Request.Protocol,
-                        ctx.Request.Method,
-                        ctx.Request.Path.ToString(),
-                        elapsedMs
-                    )
+                        let stop =
+                            Diagnostics.Stopwatch.GetTimestamp()
 
-                if result.IsNone then
-                    let! _ = defaultFunc ctx
-                    return ()
-                else
-                    return ()
-            else
+                        let elapsedMs =
+                            (double (stop - start)) * 1000.0 / freq
+
+                        let logLevel =
+                            match ctx.Response.StatusCode with
+                            | code when code < 300 -> LogLevel.Information
+                            | code when code < 500 -> LogLevel.Error
+                            | _ -> LogLevel.Critical
+
+                        logger.Log(
+                            logLevel,
+                            "Giraffe returned {Status} for {HttpProtocol} {HttpMethod} at {Path} in {ElapsedMs}",
+                            parameters =
+                                [|
+                                    ctx.Response.StatusCode
+                                    ctx.Request.Protocol
+                                    ctx.Request.Method
+                                    ctx.Request.Path.ToString()
+                                    elapsedMs
+                                |]
+                        )
+
                 return ()
-        }
+            }
 
         // Return a tupled function so it may be used from Python
         Func<_, _, _, _>(app)
